d30437a6ddfe0339a298fd1e35ef3f696beccead
laffandiPolinema <laffandi@polinema.ac.id>
2025-09-07 05:44:23 +0700

    variabel

--- bin/project1.dart
+++ bin/project1.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:project1/project1.dart' as project1;
 
 const String nama = 'Luqman';
@@ -11,4 +13,20 @@ void main(List<String> arguments) {
   //constanta dan final
   final String nama2 = 'Affandi';
   print("Nama saya $nama $nama2");
+  // variabel
+  int umur = 45;
+  double tinggi = 165.5;
+  bool jenis_kelamin = false; //true = laki-laki, false = perempuan
+  String alamat = 'Jl. Merdeka No. 45';
+  stdout.writeln("Umur saya $umur tahun");
+  stdout.writeln("Tinggi saya $tinggi cm");
+  stdout.writeln(
+    "Jenis kelamin saya ${jenis_kelamin ? 'Laki-laki' : 'Perempuan'}",
+  );
+  // if (jenis_kelamin==true) {
+  //   stdout.writeln("Jenis kelamin saya Laki-laki");
+  // } else {
+  //   stdout.writeln("Jenis kelamin saya Perempuan");
+  // }
+  stdout.writeln("Alamat saya $alamat");
 }
