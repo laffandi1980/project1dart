26b832dae06ac5690d8156e9d95ea2e2d97272f1
laffandiPolinema <laffandi@polinema.ac.id>
2025-09-07 06:10:45 +0700

    variabel lanjut list dan map

--- bin/project1.dart
+++ bin/project1.dart
@@ -29,4 +29,39 @@ void main(List<String> arguments) {
   //   stdout.writeln("Jenis kelamin saya Perempuan");
   // }
   stdout.writeln("Alamat saya $alamat");
+  num nilai = 90.5; //bisa diisi int atau double
+  stdout.writeln("Nilai saya $nilai");
+  dynamic bebas = "16";
+  stdout.writeln("Nilai dari dynamic adalah $bebas");
+  // String angka1 = "15";
+  // int angka2 = int.parse(angka1);
+  // int angka3 = bebas;
+  // list
+  List<String> hobby = ["Mancing", "Tidur", "Ngoding"];
+  hobby.add("Shopping");
+  stdout.writeln("Hobby saya adalah $hobby");
+  Map<String, String> data = {
+    "nama": "Dian",
+    "alamat": "Malang",
+    "jenis_kelamin": "Perempuan",
+  };
+  data["pekerjaan"] = "Dosen";
+  data.addAll({"hobi": "Membaca"});
+
+  stdout.writeln("Data : $data");
+  stdout.writeln("Nama yg ada di data ${data['nama']}");
+
+  try {
+    // Code that might throw an exception
+    int result = 10 ~/ 0; // This will throw an IntegerDivisionByZeroException
+  } on IntegerDivisionByZeroException {
+    // Handles the specific IntegerDivisionByZeroException
+    print("Cannot divide by zero!");
+  } catch (e) {
+    // Handles any other type of exception and provides the exception object
+    print("An unexpected error occurred: $e");
+  } finally {
+    // Code that always executes, regardless of whether an exception occurred
+    print("Exeution complete.");
+  }
 }
